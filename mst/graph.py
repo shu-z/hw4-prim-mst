f8ebd9a715706a1eb2a0ea8546b6a2882a506357
Shu Zhang <shu@broadinstitute.org>
2023-02-07 21:17:19 -0800

    add mst function

--- mst/graph.py
+++ mst/graph.py
@@ -21,13 +21,30 @@ class Graph:
             raise TypeError('Input must be a valid path or an adjacency matrix')
         self.mst = None
 
+
     def _load_adjacency_matrix_from_csv(self, path: str) -> np.ndarray:
         with open(path) as f:
             return np.loadtxt(f, delimiter=',')
 
+
+      def _get_edges(self, idx):
+        """ 
+        Takes in index of adjacency matrix and returns list of outgoing edges
+        Edges are tuples in format (edge weight, destination node)
+        
+        """
+        
+        adj=self.adj_mat
+        
+        idx_edges=np.where(adj[idx]!=0)[0]  
+        outgoing_edges=list(zip(adj[idx, idx_edges], idx_edges))
+
+        return(outgoing_edges)
+
+
+
     def construct_mst(self):
         """
-    
         TODO: Given `self.adj_mat`, the adjacency matrix of a connected undirected graph, implement Prim's 
         algorithm to construct an adjacency matrix encoding the minimum spanning tree of `self.adj_mat`. 
             
@@ -39,6 +56,47 @@ class Graph:
         of the minimum spanning tree of `self.adj_mat` in `self.mst`. We highly encourage the
         use of priority queues in your implementation. Refer to the heapq module, particularly the 
         `heapify`, `heappop`, and `heappush` functions.
-
         """
-        self.mst = None
+        adj=self.adj_mat
+
+        #init mst adjacency matrix
+        mst=np.zeros(adj.shape)
+        
+        #pick random node to start 
+        init_node=random.randint(0, adj.shape[0]-1)
+
+        #add first node to visisted      
+        visited=[init_node]
+        
+        #store all outgoing edges from visited in a priority queue
+        #heapify will work with list of tuples where tuple is (distance, node)        
+        edge_queue=self._get_edges(init_node)   
+        hq.heapify(edge_queue)
+      
+        prev_node=init_node
+        #while not all vertices in visited 
+        while len(visited)<adj.shape[0]:
+            
+            #pop lowest weight edge from priority queue 
+            edge, node=hq.heappop(edge_queue)
+
+            
+            #check if destination node of edge in visited
+            if node not in visited:
+
+                #add edge to mst
+                mst[visited[-1], node]=edge
+            
+                #add destination node to visited
+                visited.append(node)
+                
+                #get outgoing edges from node, and add to priority queue                 
+                for i in self._get_edges(node): 
+                    hq.heappush(edge_queue, i) 
+ 
+        
+        
+        #make sure mst is symmetric!
+        mst=np.triu(mst) + np.tril(mst.T, 1)
+        
+        self.mst = mst
